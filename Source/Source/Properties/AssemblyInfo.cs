4239f67d890057f1c31570e607645bcee4c56908
Sam-ie <1163429473@qq.com>
2023-07-09 08:12:31 +0800

    init project

---
+++ Source/Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyTitle("Kiiro Event")]
+[assembly: AssemblyDescription("绮罗事件拓展")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alfie Brook")]
+[assembly: AssemblyProduct("Kiiro Event")]
+[assembly: AssemblyCopyright("Copyright Alfie Brook,Baidu Tieba&Ludeon forum")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("68FD2340-E8DC-CC34-AD57-06B38DDFD6F7")]
+[assembly: AssemblyFileVersion("1.4.0")]
