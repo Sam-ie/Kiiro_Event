1e0ee9c7f894862d63a0c377f16b68eadb890554
Sam-ie <1163429473@qq.com>
2023-07-12 21:40:44 +0800

    build project

--- Source/Source/KiiroEvent.cs
+++ Source/Source/KiiroEvent.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
 
 namespace Kiiro_Event
 {
-    internal class KiiroEvent
+    [StaticConstructorOnStartup]
+    public class Kiiro_Event_Patch
     {
-
+        static Kiiro_Event_Patch()
+        {
+            Harmony harmony = new Harmony("Alfie.KiiroEvent");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
     }
 }
